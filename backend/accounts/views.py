a68475473b06a3dcdc478bdccfb8c407d0e024fc
FlyCloud_C <fly.cloud.blry@gmail.com>
2025-07-14 22:28:54 -0700

    [Backend] Create the url endpoints for Signup/Login/Token Refresh

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from .serializers import SignupSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class SignupView(generics.CreateAPIView):
+    serializer_class = SignupSerializer
+    permission_classes = [AllowAny]
